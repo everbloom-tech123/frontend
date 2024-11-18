dd717897bcebeb9ce3e1773b6f09243b5a30e7db
everbloom-tech123 <everbloomtechs@gmail.com>
2024-11-18 22:31:54 +0530

    Fixed Admin Panel

--- src/Admin_Pages/components/CategorySection.js
+++ src/Admin_Pages/components/CategorySection.js
@@ -1,27 +1,27 @@
-// Admin_Pages/components/experience/CategorySection.js
 import React from 'react';
-import { Box, Select, MenuItem, Button } from '@mui/material';
+import { Box, Select, MenuItem, Button, FormControl, InputLabel } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
 const CategorySection = ({ formData, categories, handleInputChange, onManageCategories }) => {
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center">
-      <Select
-        fullWidth
-        label="Category"
-        name="category"
-        value={formData.category}
-        onChange={handleInputChange}
-        displayEmpty
-        required
-        variant="outlined"
-        sx={{ mr: 2 }}
-      >
-        <MenuItem value="" disabled>Select Category</MenuItem>
-        {categories.map((cat) => (
-          <MenuItem key={cat.id} value={cat.name}>{cat.name}</MenuItem>
-        ))}
-      </Select>
+      <FormControl fullWidth sx={{ mr: 2 }}>
+        <InputLabel id="category-label">Category</InputLabel>
+        <Select
+          labelId="category-label"
+          id="category"
+          name="category"
+          value={formData.category || ''}
+          onChange={handleInputChange}
+          label="Category"
+          required
+        >
+          <MenuItem value="" disabled>Select Category</MenuItem>
+          {categories?.map((cat) => (
+            <MenuItem key={cat.id} value={cat.name}>{cat.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <Button 
         onClick={onManageCategories} 
         startIcon={<AddIcon />}
