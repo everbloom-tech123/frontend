e2fd6084807f2cf74dd1defe6e17917f63325412
everbloom-tech123 <everbloomtechs@gmail.com>
2025-01-01 23:45:16 +0530

    meow

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
-  //API_BASE_URL: 'https://3.83.93.102.nip.io'  // Always use production API
-  API_BASE_URL: 'http://localhost:8080'
+  API_BASE_URL: 'https://api-zdak.onrender.com'  // Always use production API
+  //API_BASE_URL: 'http://localhost:8080'
 };
 
 export default config;
