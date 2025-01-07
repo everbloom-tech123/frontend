1a3bc311ac937c49c7482621af4ec46a86d5da5d
everbloom-tech123 <everbloomtechs@gmail.com>
2025-01-08 00:22:19 +0530

    mmm

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const config = {
   //API_BASE_URL: 'https://api-zdak.onrender.com'  // Always use production API
   //API_BASE_URL: 'http://localhost:8080'
-  API_BASE_URL: 'http://43.252.15.106'  // Add the port explicitly
+  API_BASE_URL: 'https://fb31-43-252-15-106.ngrok-free.app'
 };
 
 export default config;
