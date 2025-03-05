8a955039c2bcb401bcc7261c08df2825235ddd97
everbloom-tech123 <everbloomtechs@gmail.com>
2025-03-05 18:37:49 +0000

    Update config.js (Add http://ceylonnow.com:8080/backend/)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  API_BASE_URL: 'https://api-zdak.onrender.com'  // Always use production API
+  API_BASE_URL: 'http://ceylonnow.com:8080/backend'  // Always use production API
   //API_BASE_URL: 'http://localhost:8080'
   //API_BASE_URL: 'https://fb31-43-252-15-106.ngrok-free.app'
 };
